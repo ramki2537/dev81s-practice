b04d07973a4c8bfbc97a44c34facd9962f9ab588
Rama <ramki2537@gmail.com>
2025-01-04 20:31:04 +0800

    best variable script

--- 4-array.sh
+++ 4-array.sh
@@ -6,3 +6,5 @@ echo "Best south indian industry: ${MOVIES[0]}"
 echo "Best noth indian industry: ${MOVIES[1]}"
 echo "Best english movie industry: ${MOVIES[2]}"
 echo "Best screenplay movies from: ${MOVIES[3]}"
+
+echo "All cinema industrys: ${MOVIES[@]}"
