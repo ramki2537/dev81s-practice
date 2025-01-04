f7087c8e20fa9d55b7d96a66776371df4c6fa2e1
Rama <ramki2537@gmail.com>
2025-01-04 20:29:25 +0800

    best variable script

---
+++ 4-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("telugu" "hindi" "english" "tamil")
+
+echo "Best south indian industry: ${MOVIES[0]}"
+echo "Best noth indian industry: ${MOVIES[1]}"
+echo "Best english movie industry: ${MOVIES[2]}"
+echo "Best screenplay movies from: ${MOVIES[3]}"
