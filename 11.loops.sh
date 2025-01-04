1254b46858b0bc2c15275185e46e5608d4aafa07
Rama <ramki2537@gmail.com>
2025-01-04 23:02:14 +0800

    best variable script

--- 11.loops.sh
+++ 11.loops.sh
@@ -35,7 +35,7 @@ do
    dnf list installed $package &>>$LOG_FILE_NAME
    if [ $? -ne 0 ]
    then
-      dnf install $package &>>$LOG_FILE_NAME
+      dnf install $package -y &>>$LOG_FILE_NAME
       VALIDATE $? "Installing $package"
     else
        echo -e "$package already...$Y INSTALLED $N"
