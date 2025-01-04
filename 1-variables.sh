ce5dd804f0d4d6671a494e249fe7a3717d3a64bf
Rama <ramki2537@gmail.com>
2025-01-04 20:02:24 +0800

    best variable script

---
+++ 1-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+read -p "Ener your country name: " country
+
+echo "My name is $name, i'm $age old and i belongs to $country"
