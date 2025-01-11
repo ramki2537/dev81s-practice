f9a4ce06df53b24624cb0990b9ff3f6301e26698
Rama <ramki2537@gmail.com>
2025-01-11 09:10:52 +0800

    while prac

---
+++ 12.1-delete-old.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+SOURCE_DIR="/root/app-log"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/"$LOG_FILE-$TIMESTAMP.log"
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then
+    echo "Error: You need sudo access to perform this script"
+    exit 1
+}
+
+echo "This script execute from here: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files to delete: $FILES_TO_DELETE"
+
+while read -r filepath
+do
+   echo "Deleting file: $filepath"
+   rm -rf $filepath
+   echo "Deleted: $filepath"
+done
