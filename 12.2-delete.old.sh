4fd8f1acc91f6ae066d599c5b8fb699bef4e5553
Rama <ramki2537@gmail.com>
2025-01-11 09:56:10 +0800

    while prac

--- 12.2-delete.old.sh
+++ 12.2-delete.old.sh
@@ -9,7 +9,7 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 echo "Script starts here: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "Files to be deleted: $FILES_TO_DELETE
+echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r filepath
 do
