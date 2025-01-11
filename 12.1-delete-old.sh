973b5f955b69fa2ad960561cee59fff8f4ff0edd
Rama <ramki2537@gmail.com>
2025-01-11 09:29:44 +0800

    while prac

--- 12.1-delete-old.sh
+++ 12.1-delete-old.sh
@@ -24,10 +24,10 @@ echo "This script execute from here: $TIMESTAMP" &>>$LOG_FILE_NAME
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to delete: $FILES_TO_DELETE"
 
-while read -r filepath
+while read -r filepath # here filepath is the variable name, you can give any name
 do
-   echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
-   rm -rf $filepath
-   echo "Deleted file: $filepath"
+    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+    rm -rf $filepath
+    echo "Deleted file: $filepath"
 done <<< $FILES_TO_DELETE
 
