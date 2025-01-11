7a8ecbdc23fa72c44fc6b19ec6f0023103d84165
Rama <ramki2537@gmail.com>
2025-01-11 09:37:57 +0800

    while prac

--- 12.1-delete-old.sh
+++ 12.1-delete-old.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/root/app-logs"
+SOURCE_DIR="/root/dev81s-practice/app-log"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
