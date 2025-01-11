1df16b442ac9b3c511f19bbe86116e31480e2be6
Rama <ramki2537@gmail.com>
2025-01-11 20:52:30 +0800

    while prac

--- 15.diskusage.sh
+++ 15.diskusage.sh
@@ -6,15 +6,14 @@ MSG=""
 
 while read -r line
 do
-  USAGE=$(echo $line | awk -F " " '{print$6F}' | cut -d "%" -f1)
-  PARTITION=$(echo $line | awk -F " " '{print$NF}')
-  if [ $USAGE -ge $DISK_THRESHOLD ]
-  then
-     MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
-  fi
-
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+   PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+       MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+    fi
 done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
 
-echo "$MSG | mutt -s "High Disk Usage" "rana.krish2537@gmail.com"
+echo "$MSG" | mutt -s "High Disk Usage" rana.krish2537@gmail.com
